abe487b439e832c6ad97aa94595d148677602cf3
Juan <jabritomar@gmail.com>
2024-11-08 17:08:53 +0100

    Implement languages and final login layout

--- app/src/main/java/com/example/projectmanager/MainActivity.kt
+++ app/src/main/java/com/example/projectmanager/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.projectmanager
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val loginButton = findViewById<Button>(R.id.ButtonLogin)
+
+        loginButton.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
     }
 }
